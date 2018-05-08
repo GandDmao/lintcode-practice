9d57d3cdec6b1bee3a60c6e0a5a8c4fe1b110390
GandDmao <1206622910@qq.com>
2018-05-08 15:36:51 +0800

    Add files via upload

---
+++ 454. Rectangle Area.py
@@ -0,0 +1,24 @@
+class Rectangle:
+    '''
+     * Define a constructor which expects two parameters width and height here.
+    '''
+    # write your code here
+
+    def __init__(self,width,height):
+        self.width=width
+        self.height=height
+
+    '''
+     * Define a public method `getArea` which can calculate the area of the
+     * rectangle and return.
+    '''
+    # write your code here
+    def getArea(self):
+        self.area=self.width*self.height
+        return self.area
+
+
+
+if __name__ == "__main__":
+    re = Rectangle(3,4)
+    print(re.getArea())
